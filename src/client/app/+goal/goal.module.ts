b51536fee937823c7b8cf6d2ffcf78e0c7e71050
Geoff Douglas <geoff@neverbehind.com>
2016-09-17 20:47:53 -0600

    Add in View Only Page for Goal, No Actions available.

--- src/client/app/+goal/goal.module.ts
+++ src/client/app/+goal/goal.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { GoalComponent } from './goal.component';
+import { GoalViewComponent } from './goal-view.component';
 import { GoalService, TaskService } from '../shared/goal/index';
 
 @NgModule({
     imports: [CommonModule, FormsModule, SharedModule],
-    declarations: [GoalComponent],
-    exports: [GoalComponent],
+    declarations: [GoalComponent, GoalViewComponent],
+    exports: [GoalComponent, GoalViewComponent],
     providers: [GoalService, TaskService]
 })
 
