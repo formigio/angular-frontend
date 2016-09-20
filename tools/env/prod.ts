60fe4b675e9cf78821f8646db479b9cc578a05ac
Geoff Douglas <geoff@neverbehind.com>
2016-09-19 21:28:59 -0600

    Update Prod Test API URL

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,7 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
   ENV: 'PROD',
-  API: 'https://kd32ih1imd.execute-api.us-east-1.amazonaws.com/dev'
+  API: 'https://kd32ih1imd.execute-api.us-east-1.amazonaws.com/test'
 };
 
 export = ProdConfig;
